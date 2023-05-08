bda130fe5ba86e100f9e9b46f93a7134d8a2c5e5
Felix-1871 <fellixmanning@gmail.com>
2023-05-08 23:13:00 +0200

    Remade Django

--- Django-Interface/InterfaceProject/InterfaceProject/asgi.py
+++ Django-Interface/InterfaceProject/InterfaceProject/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for mlapp project.
+ASGI config for InterfaceProject project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mlapp.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "InterfaceProject.settings")
 
 application = get_asgi_application()
--- Django-Interface/InterfaceProject/InterfaceProject/wsgi.py
+++ Django-Interface/InterfaceProject/InterfaceProject/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for mlapp project.
+WSGI config for InterfaceProject project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mlapp.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "InterfaceProject.settings")
 
 application = get_wsgi_application()
